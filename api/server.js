0202feafb332d1ccd780ea9bcee09fd7237e8167
stevenjhomem <sjhomem@outlook.com>
2021-10-11 11:42:00 -0500

    finished setting basic endpoints for the project

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
+const usersRouter = require("./users/users-router");
+const authRouter = require("./auth/auth-router");
 
 /**
   Do what needs to be done to support sessions with the `express-session` package!
@@ -21,11 +23,14 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
+
 server.get("/", (req, res) => {
   res.json({ api: "up" });
 });
 
-server.use((err, req, res, next) => { // eslint-disable-line
+server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
     message: err.message,
     stack: err.stack,
